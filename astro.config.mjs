13cd8c09636bfe03dced9caa303dbddeb129041b
Nixieboluo <me@sagirii.me>
2024-04-27 22:04:07 +0800

    feat: basic routing and i18n config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,21 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/',
+	trailingSlash: 'always',
+	build: {
+		format: 'directory',
+	},
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en', 'zh'],
+		fallback: {
+			zh: 'en',
+		},
+		routing: {
+			prefixDefaultLocale: true,
+			redirectToDefaultLocale: true,
+		},
+	},
 	integrations: [solidJs(), tailwind()],
 });
