a6c2777b0ffcadc8619c5b302970d1fa6863fff9
Nixieboluo <me@sagirii.me>
2024-05-03 23:35:47 +0800

    chore: use iconify tailwindcss plugin for icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,8 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	plugins: [require('daisyui')],
+	plugins: [addDynamicIconSelectors(), require('daisyui')],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
