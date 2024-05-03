590d20b2e89e064fdb337ce2acb2d8c36d0e6325
Nixieboluo <me@sagirii.me>
2024-05-03 23:07:38 +0800

    chore: rename layouts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,12 @@
-export type BaseLayoutProps = {
+import type { Config } from '~/config';
+
+export type LayoutBaseProps = {
 	title: string;
 	description: string;
 	lightTheme?: string;
 	darkTheme?: string;
+};
+
+export type BlankLayoutProps = LayoutBaseProps & {
+	navLinksKey: keyof typeof Config.navbarLinks;
 };
