12acfa664e7e93486415ec07a9f5593c8be8d524
Nixieboluo <me@sagirii.me>
2024-05-04 13:59:06 +0800

    feat: default value for config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,31 +5,40 @@ const configSchema = z.object({
 	 * This will be used in Astro config.
 	 */
 	i18n: z.object({
-		defaultLocale: z.string(),
-		locales: z.array(
-			z.union([
-				z.string(),
-				z.object({
-					path: z.string(),
-					codes: z.string().array(),
-				}),
-			]),
-		),
-		fallback: z.record(z.string()),
+		defaultLocale: z.string().default('en'),
+		locales: z
+			.array(
+				z.union([
+					z.string(),
+					z.object({
+						path: z.string(),
+						codes: z.string().array(),
+					}),
+				]),
+			)
+			.default(['en']),
+		fallback: z.record(z.string()).default({}),
 	}),
-	titleSeparator: z.string(),
-	icon: z.object({
-		type: z.string(),
-		href: z.string(),
-	}),
-	navbarLinks: z.record(
-		z
-			.object({
-				labelKey: z.string(),
-				href: z.string(),
-			})
-			.array(),
-	),
+	titleSeparator: z.string().default(' | '),
+	icon: z
+		.object({
+			type: z.string(),
+			href: z.string(),
+		})
+		.default({
+			type: 'image/svg+xml',
+			href: '/favicon.svg',
+		}),
+	navbarLinks: z
+		.record(
+			z
+				.object({
+					labelKey: z.string(),
+					href: z.string(),
+				})
+				.array(),
+		)
+		.default({}),
 });
 
 export type FlintConfig = z.infer<typeof configSchema>;
